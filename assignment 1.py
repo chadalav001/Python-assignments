9d2ae3f78d7a1886bb440dda0175c6c5a8e7dde5
venkata sai chowdary chadalavada <162155803+chadalav001@users.noreply.github.com>
2024-10-14 13:40:49 -0500

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,11 @@
+'''create age,height,color,you like python assign some values '''
+Name = input(("what is your name? "))
+Age = int(input("what is your age? "))
+Height = float(input("what is your Height in ft? "))
+color = input("what color do you like? ")
+print(f"Hello {Name}! "
+      f"How are you doing?"
+      f" so your age is {Age} & your height is {Height} ft"
+      f"  you like {color} color")
+
+
